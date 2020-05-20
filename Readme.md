9378ac400761906110a9a87cdb7af159ac281bad
rumothy <rumothy@yahoo.com>
2020-05-20 07:17:20 -0400

    Graph impl iter2

--- Readme.md
+++ Readme.md
@@ -2,10 +2,16 @@
 
 py -3 -m venv .venv
 
-.venv\scripts\activate
+$ .venv/Scripts/activate
+
+$ where python
+
+C:\dev\2020-05\practicepy\.venv\Scripts\python.exe
 
 We noticed a new virtual env... - yes
 
 ?powersehll: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+
 
 python -m pip install matplotlib
--- main.py
+++ main.py
@@ -1,20 +1,59 @@
+import fileinput
+from more_itertools import take, map_except
+
 class Graph:
-    def __init__(self, vertices):
-        self.__vertices = vertices;
-        self.__edges = 0;
+    def __init__(self, numVertices):
+        self.__numVertices = numVertices;
+        self.__numEdges = 0;
         self.__adjacencyLists = [];
         
-        for vertex in range(vertices):
+        for _ in range(numVertices):
             self.__adjacencyLists.append([])
 
+    @classmethod
+    def fromFile(self, file):
+        f = fileinput.input(file)
+        header = take(2, f)
+        numVertices = int(header[0])
+        numEdges = int(header[1])
+        graph = Graph(numVertices)
+        for line in take(numEdges, f):
+            vw = map_except(int, line, ValueError, TypeError)
+            vwList = list(vw)
+            v = vwList[0]
+            w = vwList[1]
+            graph.addEdge(v, w)
 
-    def Vertices(self):
-        return self.__vertices
+        fileinput.close()
+        return graph
 
-    def Edges(self):
-        return self.__edges
+    def get_numVertices(self):
+        return self.__numVertices
 
+    def get_numEdges(self):
+        return self.__numEdges
 
-graph1 = Graph(1)
-print(graph1.Vertices())
-print(graph1.Edges())
+    numVertices = property(get_numVertices)
+    numEdges = property(get_numEdges)
+
+    def addEdge(self, v, w):
+        self.__adjacencyLists[v].append(w)
+        self.__adjacencyLists[w].append(v)
+        self.__numEdges = self.__numEdges + 1
+
+    def adjacencyList(v):
+        self.__adjacencyLists[v]
+
+graph1 = Graph(6)
+graph1.addEdge(0, 5)
+graph1.addEdge(2, 4)
+graph1.addEdge(2, 3)
+graph1.addEdge(1, 2)
+graph1.addEdge(0, 1)
+graph1.addEdge(3, 4)
+graph1.addEdge(3, 5)
+graph1.addEdge(0, 2)
+
+graph2 = Graph.fromFile('tinyCG.txt')
+print(graph1.numVertices == graph2.numVertices)
+print(graph1.numEdges == graph2.numEdges)
