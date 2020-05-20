eb54e37fac1a1c963f96ef04c8532d72b8a316c8
rumothy <rumothy@yahoo.com>
2020-05-19 21:30:18 -0400

    First impl of Graph

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello")
+class Graph:
+    def __init__(self, vertices):
+        self.__vertices = vertices;
+        self.__edges = 0;
+        self.__adjacencyLists = [];
+        
+        for vertex in range(vertices):
+            self.__adjacencyLists.append([])
+
+
+    def Vertices(self):
+        return self.__vertices
+
+    def Edges(self):
+        return self.__edges
+
+
+graph1 = Graph(1)
+print(graph1.Vertices())
+print(graph1.Edges())
